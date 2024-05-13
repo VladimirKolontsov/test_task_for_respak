7fe933a9b1c2c8569b204d47e3c0fca261a323cc
Vladimir Kolontsov <kolontsovvova@mail.ru>
2024-05-13 09:13:52 +0700

    finish

--- src/main/java/ru/kolontsov/testtask/TestTask/entities/FarmerDistrict.java
+++ src/main/java/ru/kolontsov/testtask/TestTask/entities/FarmerDistrict.java
@@ -9,23 +9,35 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+/**
+ * Класс фермер-район, со всей информацией по засевным полям по одному фермеру
+ */
 @Data
 @Entity
 @Table(name = "farmer_districts")
+@Schema(example = "Farmer-District")
 public class FarmerDistrict {
 
     /**
-     * Поле id - уникальный идентификатор фермера
+     * Поле id - уникальный идентификатор записи в таблице
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Schema(name = "Unique identifier")
     private Long id;
 
+    /**
+     * Поле farmer_id - уникальный идентификатор фермера
+     */
     @Column(name = "farmer_id")
+    @Schema(name = "Farmer unique identifier")
     private Long farmer_id;
 
+    /**
+     * Поле district_id - уникальный идентификатор района
+     */
     @Column(name = "district_id")
+    @Schema(name = "District unique identifier")
     private Long district_id;
 
 }
